6de60af211b4d84a91ca6d8734300ee1752d39f6
tecnicocefet <tecnicocefet@gmail.com>
2023-11-23 12:30:31 -0300

    inclusão de arquivos

--- main.tf
+++ main.tf
@@ -13,23 +13,8 @@ resource "aws_instance" "terraform" {
     Name = "exemplo-terraform"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              sudo apt-get install software-properties-common
-              sudo  apt-add-repository -y ppa:ansible/ansible
-              sudo apt-get update
-              sudo apt-get install -y ansible
-              echo 'export PATH=$PATH:/usr/bin/ansible' >> /etc/bash.bashrc
-              # Adicionar localhost ao known_hosts
-              ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-              chmod +x /home/ubuntu/ansible/playbook.yml
-
-              # Executar o playbook Ansible com conexão local
-          
-
-              
-              EOF
+  # Carrega o conteúdo do script user_data.sh
+  user_data = file("${path.module}/user_data.sh")
 
 
   provisioner "file" {
